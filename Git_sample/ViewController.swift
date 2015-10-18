05e455ffc0a6cb7279dc6d13eb6bf3d93d3211dd
阿久津岳志 <sky_10577@yahoo.co.jp>
2015-10-18 15:15:08 +0900

    refactoring

--- Git_sample/ViewController.swift
+++ Git_sample/ViewController.swift
@@ -14,16 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
-        textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-        textLabel.backgroundColor = UIColor.redColor()
-        textLabel.text = "TECH::CAMP::Swift"
-        textLabel.font = UIFont.systemFontOfSize(40)
-        textLabel.textColor = UIColor.whiteColor()
-        textLabel.shadowColor = UIColor.blueColor()
-        textLabel.textAlignment = NSTextAlignment.Center
-        textLabel.layer.masksToBounds = true
-        textLabel.layer.cornerRadius = 20.0
+        let textLabel = makeTextLabel()
         self.view.addSubview(textLabel)
     }
 
@@ -31,7 +22,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func makeTextLabel() -> UILabel {
+        let textLabel = UILabel(frame: CGRectMake(0, 0, 300, 100))
+        textLabel.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height/2)
+        textLabel.backgroundColor = UIColor.redColor()
+        textLabel.text = "TECH::CAMP"
+        textLabel.font = UIFont.systemFontOfSize(40)
+        textLabel.textColor = UIColor.whiteColor()
+        textLabel.shadowColor = UIColor.blueColor()
+        textLabel.textAlignment = NSTextAlignment.Center
+        textLabel.layer.masksToBounds = true
+        textLabel.layer.cornerRadius = 20.0
+        return textLabel
+    }
+    
+    
 }
 
